9043374667586e817df66439405a6569e01e49f7
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-03-04 09:29:38 +0300

    Add project files.

---
+++ FirstWebApi_04_03_22/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using FirstWebApi_04_03_22.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FirstWebApi_04_03_22.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [Route("LoadProductList")]
+        public List<ProductDto> LoadProductList()
+        {
+            List<ProductDto> resultlist = new List<ProductDto>();
+        for(int i=0;i<100;i++)
+            {
+                ProductDto product = new ProductDto()
+                {
+                    ProductID = i,
+                    Brand = i + ".marka",
+                    Description = i + ".ürün açıklaması"
+                };
+                resultlist.Add(product);
+            }
+            return resultlist;
+        }
+    }
+}
---
+++ FirstWebApi_04_03_22/Models/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstWebApi_04_03_22.Models
+{
+    public class ProductDto
+    {
+        public int ProductID { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+
+
+    }
+}
